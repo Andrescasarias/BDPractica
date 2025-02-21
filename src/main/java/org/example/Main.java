4fe7a7d890a4af736d94eb5ad8a3ea81a67be649
Andrescasarias <Carlos.caca660@gmailcom>
2025-02-20 20:27:59 -0500

    salidas por consola

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("hola clase");
+        System.out.println("comandos de git que debemos aprender");
+        System.out.println("Repasando comandos ");
         }
 }
