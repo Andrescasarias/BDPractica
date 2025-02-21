1272169747523a6335fe76b6924b223e3e26865b
Andrescasarias <Carlos.caca660@gmailcom>
2025-02-20 21:22:38 -0500

    operaciones

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,17 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("hola clase");
+
         System.out.println("comandos de git que debemos aprender");
+
+        System.out.println("Repasando comandos ");
+
+        System.out.println("Repasando comandos ");
+
+        System.out.println("Repasando comandos ");
+
+        System.out.println("Repasando comandos ");
+        System.out.println("Repasando comandos ");
         System.out.println("Repasando comandos ");
         }
 }
